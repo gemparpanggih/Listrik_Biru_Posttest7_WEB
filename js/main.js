33fc5f9d996dcd99135dd254685ee34a8e60ad45
Xavier <gemparpanggih123@gmail.com>
2022-11-02 10:05:27 +0800

    update sikit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function result() {
-    let darkDiv = document.querySelectorAll('.mode');
-    for(let x of darkDiv) {
+    let darkMode = document.querySelectorAll('.mode');
+    for(let x of darkMode) {
         x.classList.toggle('dark')
     }
     alert("Mengganti Tema. Klik OK untuk melanjutkan");
